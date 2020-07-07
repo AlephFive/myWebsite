39c89238a83e7ba623ff296212a799367a0a92db
alephfive <spo2.spire.club@gmail.com>
2020-07-07 13:48:47 -0700

    Update resumeData.js

--- brianma/src/data/resumeData.js
+++ brianma/src/data/resumeData.js
@@ -5,7 +5,7 @@ export const experiences = [
     dateDescription: 'Fall 2019',
     label: 'Hulu Inc',
     location: 'Beijing, China',
-    description: 'temp asdasdasdasddte',
+    description: 'temp asdasdasdasdd',
     relavance: [],
     tags: [],
   },
