4ef1086920592fc484cbe200d6e1da7e7ff3143d
Shao-en Ma <bm3027@columbia.edu>
2020-12-15 08:11:23 -0800

    Update TempProjPage.js

--- src/pages/TempProjPage.js
+++ src/pages/TempProjPage.js
@@ -25,7 +25,7 @@ class TempProjPage extends React.Component {
         <br />
         <Row>
           <Col sm={{ span: 2, offset: 2 }}>
-            <Image src="me.jpg" rounded fluid />
+            <h1>Temporary Proj</h1>
           </Col>
           <Col sm={6} style={{ textAlign: 'left' }}>
           <iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FmJqovaVeAwKNaweQ3GFpzW%2FUI-Design-Final%3Fnode-id%3D34%253A133%26scaling%3Dscale-down" allowfullscreen></iframe>
