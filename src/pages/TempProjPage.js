3166fac9f80abb4277f83aea4bc6ca71af909bbe
Shao-en Ma <bm3027@columbia.edu>
2021-01-23 18:42:22 -0500

    testing iframes

--- src/pages/TempProjPage.js
+++ src/pages/TempProjPage.js
@@ -28,7 +28,7 @@ class TempProjPage extends React.Component {
             <h1>Temporary Proj</h1>
           </Col>
           <Col sm={6} style={{ textAlign: 'left' }}>
-          
+          <iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FmJqovaVeAwKNaweQ3GFpzW%2FUI-Design-Final%3Fnode-id%3D34%253A133%26scaling%3Dscale-down" allowfullscreen></iframe>
             <br />
           </Col>
         </Row>
@@ -38,6 +38,6 @@ class TempProjPage extends React.Component {
   }
 }
 
-//<iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FmJqovaVeAwKNaweQ3GFpzW%2FUI-Design-Final%3Fnode-id%3D34%253A133%26scaling%3Dscale-down" allowfullscreen></iframe>
+//
 
 export default TempProjPage;
