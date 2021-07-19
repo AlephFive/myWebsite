9f5d7c50b8f33947e10c19f308cc0c3ee0654fb4
Shao-en Ma <bm3027@columbia.edu>
2021-07-19 14:12:27 -0400

    Update contactData.js

--- src/data/contactData.js
+++ src/data/contactData.js
@@ -2,6 +2,7 @@ export const contactInfo = {
   phone: '+1 425-524-6671',
   email: 'bm3027@columbia.edu or bma@colgate.edu',
   addr: [
+    { label: 'Name', value: 'Brian Ma' },
     { label: 'Line 1', value: '70 Morningside' },
     { label: 'Line 2', value: 'F1554 Wien Hall Columbia Student Mail' },
     { label: 'City-State', value: 'New York, NY' },
