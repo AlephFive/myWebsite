27320b07381232705a52567fcf6742d386c11998
Shao-en Ma <bm3027@columbia.edu>
2021-09-29 16:48:22 -0400

    Update filter order to focus on design

--- src/data/TagReference.js
+++ src/data/TagReference.js
@@ -9,12 +9,12 @@ export const FILTERS = [
   'communication',
 ];
 export const FILTERNAMES = {
+  design: 'Design',
+  research: 'Research',
+  leadership: 'Leadership',
+  communication: 'Communication',
+  data: 'Data Analysis',
   software: 'Software',
   hardware: 'Hardware',
-  data: 'Data Analysis',
   maths: 'Mathematics',
-  leadership: 'Leadership',
-  research: 'Research',
-  design: 'Design',
-  communication: 'Communication',
 };
